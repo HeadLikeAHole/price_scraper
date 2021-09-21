208b939f11eceeb8c92aec2a8ec4a598eca87137
igor <igorwho@yandex.ru>
2021-09-21 14:43:43 +0300

    Starting up

--- src/__init__.py
+++ src/__init__.py
@@ -8,3 +8,15 @@ app.config.from_pyfile('config.py')
 
 # Create database connection object
 db = SQLAlchemy(app)
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+# Create app
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+# Create database connection object
+db = SQLAlchemy(app)
+
+from . import routes
