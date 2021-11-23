165353f181e23b92a0d28d145ae3e62fe5b07099
Igor <igorwho@yandex.ru>
2021-11-23 22:21:05 +0300

    Making main page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,39 @@
+import runningMan from '../img/running-man.png';
+import dotMap from '../img/dot-map.png';
+
 export default function Home() {
   return (
-    <div className="">
-    </div>
+    <main>
+      <section className="main-first">
+        <div className="main-first-text">
+          <h1 className="main-first-title">
+            An easy way to keep track of prices on your favorite online stores
+          </h1>
+          <p className="main-first-description">
+            Just save the link to the product you wish to buy and get an email when the price drops to the amount you specified
+          </p>
+        </div>
+        <div className="running-man">
+          <img src={runningMan} alt="" />
+        </div>
+      </section>
+      <section className="main-second">
+        <h1 className="main-second-title">
+          To get started just create an account
+        </h1>
+        <article>
+          <h2></h2>
+          <p></p>
+        </article>
+        <article>
+          <h2></h2>
+          <p></p>
+        </article>
+        <article>
+          <h2></h2>
+          <p></p>
+        </article>
+      </section>
+    </main>
   );
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,15 @@
+import logo from '../img/logo.png';
+
 export default function Navbar() {
   return (
     <nav>
-      <ul>
-        <img src="./logo.png" alt=""/>
-        <li>
-          text
-        </li>
-      </ul>
+      <div className="logo">
+        <img src={logo} alt="" title="Price Scraper"/>
+      </div>
+      <div className="nav-buttons">
+        <button className="btn btn-nav">log in</button>
+        <button className="btn btn-nav">sign up</button>
+      </div>
     </nav>
   );
 }
