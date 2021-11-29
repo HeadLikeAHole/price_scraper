8f9af1431315d389d0656e0449f6008100bdc841
Igor <igorwho@yandex.ru>
2021-11-29 20:34:04 +0300

    Making second section

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,10 +7,10 @@ export default function Home() {
       <section className="main-first">
         <div className="main-first-text">
           <h1 className="main-first-title">
-            An easy way to keep track of prices on your favorite online stores
+            An easy way to keep track of <span>prices</span> on your favorite online <span>stores</span>
           </h1>
           <p className="main-first-description">
-            Just save the link to the product you wish to buy and get an email when the price drops to the amount you specified
+            Save the link to the product you wish to buy and get an email when the price drops to the amount you specified
           </p>
         </div>
         <div className="running-man">
@@ -21,18 +21,20 @@ export default function Home() {
         <h1 className="main-second-title">
           To get started just create an account
         </h1>
-        <article>
-          <h2></h2>
-          <p></p>
-        </article>
-        <article>
-          <h2></h2>
-          <p></p>
-        </article>
-        <article>
-          <h2></h2>
-          <p></p>
-        </article>
+        <div className="articles">
+          <article>
+            <h2 className="article-title">Easy to sign up</h2>
+            <p className="article-description">Just provide your email and password and you're good to go</p>
+          </article>
+          <article>
+            <h2 className="article-title">Simple to use</h2>
+            <p className="article-description">Find a product you want to buy, save the link to it and set a desired price</p>
+          </article>
+          <article>
+            <h2 className="article-title">Constant updates</h2>
+            <p className="article-description">If the price drops to the desired one get a notifying email within an hour</p>
+          </article>
+        </div>
       </section>
     </main>
   );
